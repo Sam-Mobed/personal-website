3830fbf5da585e42979123ec6b13ac2ccb59b1a8
Sam-Mobed <masdebom@gmail.com>
2023-08-05 15:07:28 -0400

    body

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/Hero'
+import Text from '@/components/Text'
 
 export default function Home() {
   return (
     <main className='h-[200vh] bg-black overflow-hidden'>
       <Hero />
+      <Text />
     </main>
   )
 }
---
+++ components/Text/index.tsx
@@ -0,0 +1,16 @@
+import './text.css'
+import {GiCardJoker} from 'react-icons/gi'
+
+export default function Text(){
+    return (
+        <div className="ml-[5vmax] mr-[5vmax] mt-[17vw]">
+            <div className='flex justify-between items-center'>
+                <h1 className="text-white">Sam</h1>
+                <h1 className='text-white'>❧</h1>
+            </div>
+            <h1 className="text-white title">Mobed</h1>
+            
+            <h2 className='text-white pt-5'>Welcome to my Digital Portfolio</h2>
+        </div>
+    )
+}
